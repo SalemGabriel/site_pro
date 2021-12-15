caa33feb91e96ad59b25afad153a4fc33a72719e
Gabriel Salem <90392582+SalemGabriel@users.noreply.github.com>
2021-12-15 15:59:13 +0100

    foireux

--- PHP/competences.php
+++ PHP/competences.php
@@ -1,4 +1,13 @@
 <?php
 $data=yaml_parse_file("yaml/competences.yaml");
-echo $data["competences"];
+echo '<div class="text">';
+echo $data["orga"];
+echo "<br>";
+echo $data["anim"];
+echo "<br>";
+echo $data["ser"];
+echo "<br>";
+echo $data["trav"];
+echo "<br>";
+echo '</div>'
 ?>
