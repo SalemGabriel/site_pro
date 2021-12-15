9be871920833491c7fa9be3b9be90d612db82729
Gabriel Salem <90392582+SalemGabriel@users.noreply.github.com>
2021-12-15 16:09:31 +0100

    retour en arrière

--- PHP/competences.php
+++ PHP/competences.php
@@ -1,13 +1,4 @@
 <?php
 $data=yaml_parse_file("yaml/competences.yaml");
-echo '<div class="text">';
-echo $data["orga"];
-echo "<br>";
-echo $data["anim"];
-echo "<br>";
-echo $data["ser"];
-echo "<br>";
-echo $data["trav"];
-echo "<br>";
-echo '</div>'
+echo $data["competences"];
 ?>
